ba5b4d0eee4c73f7871308057a5ebea9824e965a
dampuzakura <168283013+dampuzakura@users.noreply.github.com>
2024-10-17 01:17:29 +0900

    ✨ 設定用ファイルを切り出し

---
+++ index.ts
@@ -0,0 +1,114 @@
+/*
+ * Please set the following variables to your liking!
+ * You can use Fluent UI System Icons and Simple Icons for the icons!
+ * Fluent UI System Icons: https://icones.js.org/collection/fluent
+ * Simple Icons: https://icones.js.org/collection/simple-icons
+ */
+export const name = 'Dampuzakura';
+export const introduction =
+  'I am a high school student from Japan who enjoys developing and taking photos.';
+export const avatar = '/images/avatar.png';
+export const about =
+  'I am a high school student from Japan. I enjoy developing and taking photos. I am not very good at creating websites, but I did my best to make this one. I usually hang out on Mastodon. Please follow me there. I want to make many friends and get along with everyone. Thank you for reading this site!';
+export const place = 'Japan';
+export const timezone = 'Asia/Tokyo +9:00';
+export const experiences = [
+  {
+    title: 'Developing',
+    description:
+      'I have created several websites and web applications using Astro and other technologies. This website is also built with Astro! Additionally, it is styled with UnoCSS!',
+  },
+  {
+    title: 'Photography',
+    description:
+      'I usually take various photos and post them on Mastodon. You can check out the photos I have taken on my Mastodon!',
+  },
+  {
+    title: 'Writing',
+    description:
+      "I haven't written many articles. Currently, I am creating a website to write a blog, so please look forward to it!",
+  },
+];
+export const skills = [
+  { name: 'HTML', icon: 'i-simple-icons:html5' },
+  { name: 'CSS', icon: 'i-simple-icons:css3' },
+  { name: 'UnoCSS', icon: 'i-simple-icons:unocss' },
+  { name: 'JavaScript', icon: 'i-simple-icons:javascript' },
+  { name: 'TypeScript', icon: 'i-simple-icons:typescript' },
+  { name: 'Svelte', icon: 'i-simple-icons:svelte' },
+  { name: 'Astro', icon: 'i-simple-icons:astro' },
+  { name: 'Golang', icon: 'i-simple-icons:go' },
+  { name: 'Python', icon: 'i-simple-icons:python' },
+  { name: 'Git', icon: 'i-simple-icons:git' },
+  { name: 'GitHub', icon: 'i-simple-icons:github' },
+  { name: 'VSCode', icon: 'i-simple-icons:visualstudiocode' },
+  { name: 'Powershell', icon: 'i-simple-icons:powershell' },
+  { name: 'Windows', icon: 'i-simple-icons:windows' },
+  { name: 'Linux', icon: 'i-simple-icons:linux' },
+  { name: 'etc...', icon: '' },
+];
+export const works = [
+  {
+    title: "Dampuzakura's Website",
+    description:
+      'This website is my portfolio site. I created this site to introduce myself and my works. I hope you enjoy it!',
+    url: 'https://dampuzakura.github.io',
+    linkText: 'Visit',
+  },
+  {
+    title: "Dampuzakura's Blog",
+    description:
+      'I am currently creating a blog site. I will write articles about various things there. Please look forward to it!',
+    url: '',
+    linkText: '',
+  },
+  {
+    title: 'DZ Quick Card',
+    description:
+      'DZ Quick Card is a simple web application that allows you to create a business card. You can create a card with your name and a message. Please try it!',
+    url: 'https://dampuzakura.github.io/dz-quick-card',
+    linkText: 'Try it!',
+  },
+];
+export const socials = [
+  {
+    name: 'dampuzakura.github.io',
+    url: 'https://dampuzakura.github.io',
+    icon: 'i-fluent:link-48-regular',
+  },
+  {
+    name: '@dampuzakura@fedibird.com',
+    url: 'https://fedibird.com/@dampuzakura',
+    icon: 'i-simple-icons:mastodon',
+  },
+  {
+    name: '@dampuzakura',
+    url: 'https://github.com/dampuzakura',
+    icon: 'i-simple-icons:github',
+  },
+  {
+    name: '@dampuzakura',
+    url: 'https://x.com/dampuzakura',
+    icon: 'i-simple-icons:x',
+  },
+  {
+    name: '@dampuzakura.github.io',
+    url: 'https://bsky.app/profile/dampuzakura.github.io',
+    icon: 'i-simple-icons:bluesky',
+  },
+  {
+    name: '@dampuzakura@fedimovie.com',
+    url: 'https://fedimovie.com/@dampuzakura',
+    icon: 'i-simple-icons:peertube',
+  },
+  {
+    name: '@dampuzakura@fedisnap.com',
+    url: 'https://fedisnap.com/@dampuzakura',
+    icon: 'i-simple-icons:pixelfed',
+  },
+  {
+    name: '@dampuzakura@misskey.io',
+    url: 'https://misskey.io/@dampuzakura',
+    icon: 'i-simple-icons:misskey',
+  },
+];
