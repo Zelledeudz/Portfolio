f9fde664811d990164db908b77da6906dc8aa16d
dampuzakura <168283013+dampuzakura@users.noreply.github.com>
2024-10-16 22:10:15 +0900

    💄 ナビゲーションバーのデザイン変更

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,64 +1,53 @@
 <script lang="ts">
   const navItems = [
     {
-      href: '/',
+      href: '#',
       icon: 'i-fluent:home-48-regular',
       label: 'Home',
-      tooltip: 'Go to Home',
     },
     {
-      href: '/about',
+      href: '#about',
       icon: 'i-fluent:info-48-regular',
       label: 'About',
-      tooltip: 'Learn more about us',
     },
     {
-      href: '/skills',
+      href: '#skills',
       icon: 'i-fluent:toolbox-32-regular',
       label: 'Skills',
-      tooltip: 'Our skills',
     },
     {
-      href: '/experiences',
+      href: '#experiences',
       icon: 'i-fluent:briefcase-48-regular',
       label: 'Experiences',
-      tooltip: 'Our experiences',
     },
     {
-      href: '/works',
+      href: '#works',
       icon: 'i-fluent:content-view-gallery-28-regular',
       label: 'Works',
-      tooltip: 'Our works',
     },
     {
-      href: '/followme',
+      href: '#socials',
       icon: 'i-fluent:people-add-32-regular',
-      label: 'Follow Me',
-      tooltip: 'Follow me on social media',
+      label: 'Socials',
     }
   ];
 </script>
 
 <nav
-  class="fixed bottom-0 w-full text-black bg-white shadow-xl dark:text-white dark:bg-gray-9 md:left-0 md:top-0 md:w-50 backdrop-blur-lg md:my-8 rounded-t-2xl md:border-0 md:rounded-lt-0 md:rounded-r-3xl"
+  class="fixed bottom-0 w-full h-16 text-white shadow-xl md:h-full dark:text-gray-1 bg-gradient-to-r from-blue-6 to-purple-6 from-blue-9 to-purple-9 md:left-0 md:top-0 md:w-50 backdrop-blur-lg rounded-t-2xl md:border-0 md:rounded-lt-0 md:rounded-r-3xl z-99"
 >
   <ul
     class="flex flex-row justify-center w-full h-full md:flex-col gap-x-4 md:gap-y-2"
   >
-    {#each navItems as { href, icon, label, tooltip }}
+    {#each navItems as { href, icon, label }}
       <li class="relative flex items-center group">
         <a
-          {href}
-          class="flex items-center w-full h-full py-4 text-blue-8 dark:text-blue-4 text-6 md:gap-x-4 md:py-0 md:px-4 hover:filter hover:text-red-8 dark:hover:text-red-4 transition-300"
+          href={href}
+          class="flex items-center w-full h-full py-2 transition-all duration-300 ease-in-out transform rounded-md text-6 md:gap-x-4 md:py-0 md:px-4 hover:font-bold group-hover:scale-110"
         >
-          <span class={icon + ' w-1em h-1em block'}></span>
+          <span class={icon + ' w-8 h-8 md:w-6 md:h-6 block'}></span>
           <span class="hidden md:block">{label}</span>
         </a>
-        <span
-          class="absolute hidden px-2 py-1 mb-2 text-xs text-white transform -translate-x-1/2 bg-gray-700 rounded dark:bg-gray-9 bottom-full left-1/2 group-hover:block"
-        >
-          {tooltip}
-        </span>
       </li>
     {/each}
   </ul>
